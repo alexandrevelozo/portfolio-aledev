eaa4dd913745ff80d80b6906c575fdb61ce243a4
Alexandre Velozo <alexandrevelozo4@gmail.com>
2023-03-02 20:28:39 -0300

    fix: bugs source map

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Services from "./components/Services";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 
+// caso der erro no react scrips
+// "start": "react-scripts --openssl-legacy-provider start",
+
 const App = () => {
   return (
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
